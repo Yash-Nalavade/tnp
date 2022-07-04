5a18cd7505e467abeeab40731bfdbc3887b5f27c
YashNalavade <yashnalawade1516@gmail.com>
2022-07-04 23:19:53 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(tnp());
+}
+
+MaterialApp tnp(){
+  return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Training and Placement'),),
+      )
+  );
+}
